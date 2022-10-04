fd79afbe3fd3e6f17c39c687e0eec6bcb58f5cb3
mehmetserkanyilmazz <mehmetserkanyilmazz@gmail.com>
2022-10-04 16:35:08 +0300

    class has been modified.

---
+++ src/main/java/org/leetCode/TwoSum.java
@@ -0,0 +1,13 @@
+package main.java.org.leetCode;
+
+public class TwoSum {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+    }
+    public int[] twoSum(int[] nums, int target) {
+        return null;
+    }
+}
+
